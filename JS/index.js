55f35435125e5db7b579a52110223881e981c285
Nathaniel Tang <nathanieltang22@gmail.com>
2022-07-19 00:27:45 -0400

    created image carousel HTML and CSS; JS for test

--- JS/index.js
+++ JS/index.js
@@ -4,3 +4,11 @@ const dropdown = document.querySelector('.dropdownul')
 gallery.addEventListener('click', () => dropdown.classList.add('active'))
 
 dropdown.addEventListener('click', () => dropdown.classList.remove('active'))
+
+const carousel = document.querySelector('.carousel');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+let translateX = -900;
+
+prev.addEventListener("click", () => carousel.style.transform = `translateX(${translateX}px)`)
+next.addEventListener("click", () => carousel.style.transform = "translateX(-1800px)")
