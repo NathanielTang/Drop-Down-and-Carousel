dafbf1aec1d1c19e75285d72234e1e6c5aa65d63
Nathaniel Tang <nathanieltang22@gmail.com>
2022-07-29 11:29:33 -0400

    created functions to move the carousel left and right

--- JS/index.js
+++ JS/index.js
@@ -8,7 +8,32 @@ dropdown.addEventListener('click', () => dropdown.classList.remove('active'))
 const carousel = document.querySelector('.carousel');
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
-let translateX = -900;
+const frame = document.querySelector('.frame')
 
-prev.addEventListener("click", () => carousel.style.transform = `translateX(${translateX}px)`)
-next.addEventListener("click", () => carousel.style.transform = "translateX(-1800px)")
+const frameWidth= frame.getBoundingClientRect().width;
+
+
+prev.addEventListener("click", moveToNext)
+next.addEventListener("click", moveToPrev)
+
+let amountToMove = 0;
+
+function moveCarousel(direction) {
+    
+    if (direction === "next") {
+        amountToMove -= frameWidth;
+    }
+    if (direction === "prev") {
+        amountToMove += frameWidth;
+    }
+    
+    carousel.style.transform = `translateX(${amountToMove}px)`;
+}
+
+function moveToNext() {
+    moveCarousel("next")
+}
+
+function moveToPrev() {
+    moveCarousel("prev")
+}
