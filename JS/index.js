a599bb44e379fc0ffc760ef89c1bfa5b43f00fbe
Nathaniel Tang <nathanieltang22@gmail.com>
2022-08-18 21:17:48 -0400

    added ability to toggle between galleries of different things

--- JS/index.js
+++ JS/index.js
@@ -12,7 +12,6 @@ const frame = document.querySelector('.frame')
 
 // const frameWidth= frame.getBoundingClientRect().width;
 const frameWidth = 900;
-console.log(frameWidth)
 
 prev.addEventListener("click", moveToPrev)
 next.addEventListener("click", moveToNext)
@@ -62,4 +61,59 @@ function moveToNext() {
 function moveToPrev() {
     
     changeCurrentSlide("prev")
+}
+
+
+//change cats to dogs using gallery dropdown buttons
+
+const cat = document.getElementById('dropdown-cat')
+const dog = document.getElementById('dropdown-dog')
+const xdd = document.getElementById('dropdown-xdd')
+
+cat.addEventListener('click', changeToCat)
+dog.addEventListener('click', changeToDog)
+xdd.addEventListener('click', changeToXdd)
+
+function changeToCat() {
+    changeGallery('cat')
+    console.log('i am cat')
+}
+
+function changeToDog() {
+    changeGallery('dog')
+    console.log('i am doge')
+}
+
+function changeToXdd() {
+    changeGallery('xdd')
+    console.log('xdders')
+}
+
+const changeGallery = (animal) => {
+    const image1 = document.getElementById('image-1');
+    const image2 = document.getElementById('image-2');
+    const image3 = document.getElementById('image-3');
+    console.log(animal)
+
+    if (animal === 'cat') {
+        image1.src="./Images/Cat1.jpg";
+        image2.src="./Images/Cat2.jpg";
+        image3.src="./Images/Cat3.jpg";
+        console.log('kitty')
+        return   
+    }
+    if (animal === 'dog') {
+        image1.src="./Images/Dog1.jpg";
+        image2.src="./Images/Dog2.jpg";
+        image3.src="./Images/Dog3.jpg";
+        console.log('doggy')
+        return    
+    }
+    if (animal === 'xdd') {
+        image1.src="./Images/xdd1.png";
+        image2.src="./Images/xdd2.webp";
+        image3.src="./Images/xdd3.jpg";
+        console.log('xddddddddddd')
+        return    
+    }
 }
